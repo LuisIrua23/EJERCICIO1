c9302efbd496192b01cc8307aea780a442901298
LuisIrua23 <lairua@soy.sena.edu.co>
2024-06-17 00:26:11 -0500

    1

--- resources/views/module_carpet/listar.blade.php
+++ resources/views/module_carpet/listar.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
+    <title>Modulos</title>
 </head>
 <body>
     <!-- <center>
